12c1da1239e5e36f84e7a9fe28e2b03b8fee10d3
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-03-17 23:39:06 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+$routes->get('/home', function(){
+    return view('welcome_message');
+});
 $routes->get('/', function(){
     return view('dao_view');
 });
